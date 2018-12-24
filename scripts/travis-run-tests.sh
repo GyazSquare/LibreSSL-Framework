d37792df71343afe6560e754c0c9293878c7ec6c
Goichi Hirakawa <gooichi@gyazsquare.com>
2018-12-24 21:10:17 +0900

    refs #73 Add the destination option in travis-run-tests.sh

--- scripts/travis-run-tests.sh
+++ scripts/travis-run-tests.sh
@@ -4,7 +4,7 @@ set -ev
 set -o pipefail
 
 if [ "${TRAVIS_XCODE_PROJECT-UNDEF}" != "UNDEF" ]; then
-    xcodebuild -project ${TRAVIS_XCODE_PROJECT} -scheme ${TRAVIS_XCODE_SCHEME} -sdk ${TRAVIS_XCODE_SDK} test
+    xcodebuild -project ${TRAVIS_XCODE_PROJECT} -scheme ${TRAVIS_XCODE_SCHEME} -destination=macOS -sdk ${TRAVIS_XCODE_SDK} test
 else
-    xcodebuild -workspace ${TRAVIS_XCODE_WORKSPACE} -scheme ${TRAVIS_XCODE_SCHEME} -sdk ${TRAVIS_XCODE_SDK} test
+    xcodebuild -workspace ${TRAVIS_XCODE_WORKSPACE} -scheme ${TRAVIS_XCODE_SCHEME} -destination=macOS -sdk ${TRAVIS_XCODE_SDK} test
 fi
