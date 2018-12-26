aac85f0277bafbb7b4058c7dcba2e11916e81fa7
Goichi Hirakawa <gooichi@gyazsquare.com>
2018-12-26 21:51:46 +0900

    refs #73 Fix xcodebuild destination specifiers

--- scripts/travis-run-tests.sh
+++ scripts/travis-run-tests.sh
@@ -3,8 +3,10 @@
 set -ev
 set -o pipefail
 
+DESTINATION="'platform=macOS'"
+
 if [ "${TRAVIS_XCODE_PROJECT-UNDEF}" != "UNDEF" ]; then
-    xcodebuild -project ${TRAVIS_XCODE_PROJECT} -scheme ${TRAVIS_XCODE_SCHEME} -destination=macOS -sdk ${TRAVIS_XCODE_SDK} test
+    xcodebuild -project ${TRAVIS_XCODE_PROJECT} -scheme ${TRAVIS_XCODE_SCHEME} -destination ${DESTINATION} -sdk ${TRAVIS_XCODE_SDK} test
 else
-    xcodebuild -workspace ${TRAVIS_XCODE_WORKSPACE} -scheme ${TRAVIS_XCODE_SCHEME} -destination=macOS -sdk ${TRAVIS_XCODE_SDK} test
+    xcodebuild -workspace ${TRAVIS_XCODE_WORKSPACE} -scheme ${TRAVIS_XCODE_SCHEME} -destination ${DESTINATION} -sdk ${TRAVIS_XCODE_SDK} test
 fi
