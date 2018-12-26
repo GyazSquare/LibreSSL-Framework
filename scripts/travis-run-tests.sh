0484494b5a06a7241b2a39727d05a8af997e74f3
Goichi Hirakawa <gooichi@gyazsquare.com>
2018-12-26 22:03:54 +0900

    refs #73 Remove xcodebuild destination specifiers

--- scripts/travis-run-tests.sh
+++ scripts/travis-run-tests.sh
@@ -3,10 +3,8 @@
 set -ev
 set -o pipefail
 
-DESTINATION="'platform=macOS'"
-
 if [ "${TRAVIS_XCODE_PROJECT-UNDEF}" != "UNDEF" ]; then
-    xcodebuild -project ${TRAVIS_XCODE_PROJECT} -scheme ${TRAVIS_XCODE_SCHEME} -destination ${DESTINATION} -sdk ${TRAVIS_XCODE_SDK} test
+    xcodebuild -project ${TRAVIS_XCODE_PROJECT} -scheme ${TRAVIS_XCODE_SCHEME} -sdk ${TRAVIS_XCODE_SDK} test
 else
-    xcodebuild -workspace ${TRAVIS_XCODE_WORKSPACE} -scheme ${TRAVIS_XCODE_SCHEME} -destination ${DESTINATION} -sdk ${TRAVIS_XCODE_SDK} test
+    xcodebuild -workspace ${TRAVIS_XCODE_WORKSPACE} -scheme ${TRAVIS_XCODE_SCHEME} -sdk ${TRAVIS_XCODE_SDK} test
 fi
