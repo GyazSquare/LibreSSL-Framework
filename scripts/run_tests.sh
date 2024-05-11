f6814261b382a96cdd3196545d61d797fb865b0d
Goichi Hirakawa <gooichi@gyazsquare.com>
2024-05-11 19:45:57 +0900

    refs #123 Add XCODE_DESTINATION environment

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,7 +3,7 @@
 set -ev
 
 if [ "${XCODE_PROJECT-UNDEF}" != "UNDEF" ]; then
-    xcodebuild -project ${XCODE_PROJECT} -scheme ${XCODE_SCHEME} -sdk ${XCODE_SDK} test
+    xcodebuild -project ${XCODE_PROJECT} -scheme ${XCODE_SCHEME} -destination ${XCODE_DESTINATION} -sdk ${XCODE_SDK} test
 else
-    xcodebuild -workspace ${XCODE_WORKSPACE} -scheme ${XCODE_SCHEME} -sdk ${XCODE_SDK} test
+    xcodebuild -workspace ${XCODE_WORKSPACE} -scheme ${XCODE_SCHEME} -destination ${XCODE_DESTINATION} -sdk ${XCODE_SDK} test
 fi
